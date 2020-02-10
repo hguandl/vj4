4c59e6fc00e7c98057bf1694a4ef530186339489
Hao Guan <hguandl@gmail.com>
2020-02-10 22:23:46 +0800

    fix CAS URL

--- vj4/util/casclient.py
+++ vj4/util/casclient.py
@@ -12,7 +12,7 @@ async def __fetch(url):
 
 async def get_user_info(ticket):
     response = await __fetch(
-        f'https://cas.sustc.edu.cn/cas/p3/serviceValidate?service='
+        f'https://cas.sustech.edu.cn/cas/p3/serviceValidate?service='
         f'{options.url_prefix}/auth/login&format=json&ticket={ticket}')
     try:
         return json.loads(response)['serviceResponse']['authenticationSuccess']['attributes']
