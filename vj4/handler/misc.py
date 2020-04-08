f4607ccc4b735d6d1fd68d280dbf89b53fd45e77
Hao Guan <hguandl@gmail.com>
2020-04-08 21:43:58 +0800

    status page

--- vj4/handler/misc.py
+++ vj4/handler/misc.py
@@ -1,7 +1,9 @@
 from vj4 import app
 from vj4.handler import base
 from vj4.util import misc
+from vj4.model import record
 
+from datetime import datetime
 
 @app.route('/about', 'about', global_route=True)
 class AboutHandler(base.Handler):
@@ -22,3 +24,28 @@ class PreviewHandler(base.Handler):
   async def post(self, *, text: str):
     self.response.content_type = 'text/html'
     self.response.text = misc.markdown(text)
+
+
+@app.route('/status/waiting', 'status', global_route=True)
+class WaitingStatusHandler(base.Handler):
+  async def get(self):
+    waiting_records = []
+    waiting = record.get_multi(get_hidden=True, status={'$eq': 0})\
+                    .sort('judge_at', -1)\
+                    .limit(5)
+    async for r in waiting:
+      waiting_records.append(r['judge_at'])
+    last_waiting_time = waiting_records[0] if len(waiting_records) > 0 else 0
+
+    now = datetime.now()
+    if len(waiting_records) >= 5 or now.timestamp() - last_waiting_time <= 300:
+      should_restart = True
+    else:
+      should_restart = False
+
+    resp = {
+      'waiting_count': len(waiting_records),
+      'last_waiting_time': last_waiting_time,
+      'should_restart': should_restart
+    }
+    self.json(resp)
